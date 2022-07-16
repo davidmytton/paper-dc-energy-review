0bf637c210ac71c7f8a8d431163cf73d0fa1ce45
David Mytton <david@davidmytton.co.uk>
2022-07-16 17:04:51 +0100

    Sync summary with manuscript

--- app.py
+++ app.py
@@ -542,22 +542,22 @@ app.layout = html.Div(
 # Summary
 
 > Data centers are a critical component of Information Technology (IT),
-> providing an environment for running computer equipment. Reliance on data
-> centers for everyday activities has seen increased scrutiny of their energy
-> footprint, yet the literature presents a wide range of estimates with
-> challenging to validate calculations that makes it difficult to rely on their
-> subsequent estimates. In this review, we analyze 46 original data center
-> energy estimates published between 2007 and 2021 to assess their reliability
-> through examining the 676 sources used. We show that 31% of sources were from
-> peer-reviewed publications, 38% were from non-peer reviewed reports, and many
-> lacked clear methodologies and data provenance. We also highlight issues with
-> source availability - there is a reliance on private data from IDC (43%) and
-> Cisco (30%), 11% of sources had broken web links, and 10% were cited with
-> insufficient detail to locate. We make recommendations to 3 groups of
-> stakeholders for how to improve and better understand the literature -
-> end-users who make use of data center energy estimates e.g. journalists; the
-> research community e.g. academics; and policy-makers or regulators within the
-> energy sector e.g. grid operators.
+providing an environment for running computer equipment. Reliance on data
+centers for everyday activities has seen increased scrutiny of their energy
+footprint, yet the literature presents a wide range of estimates with
+challenging to validate calculations that makes it difficult to rely on their
+subsequent estimates. In this review, we analyze 258 data center energy
+estimates from 46 original publications between 2007 and 2021 to assess their
+reliability through examining the 676 sources used. We show that 31% of sources
+were from peer-reviewed publications, 38% were from non-peer reviewed reports,
+and many lacked clear methodologies and data provenance. We also highlight
+issues with source availability - there is a reliance on private data from IDC
+(43%) and Cisco (30%), 11% of sources had broken web links, and 10% were cited
+with insufficient detail to locate. We make recommendations to 3 groups of
+stakeholders for how to improve and better understand the literature -
+end-users who make use of data center energy estimates e.g. journalists; the
+research community e.g. academics; and policy-makers or regulators within the
+energy sector e.g. grid operators.
     """
         ),
         html.H2("Stats"),
